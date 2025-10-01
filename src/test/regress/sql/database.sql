33a998392e21b6eeb0739787abc62e867ca6e3f8
Akshay Joshi <akshay.joshi@enterprisedb.com>
2025-10-01 16:08:48 +0530

    Remove separate test case files for the database and use the existing ones instead.
    Remove extra lines.

--- src/test/regress/sql/database.sql
+++ src/test/regress/sql/database.sql
@@ -24,14 +24,12 @@ DROP ROLE regress_datdba_before;
 DROP ROLE regress_datdba_after;
 
 -- Without Pretty formatted
-
 -- Create a specific role to test
 CREATE ROLE test_database_ddl_role WITH SUPERUSER;
 CREATE DATABASE "test_get_database_ddl"
     OWNER test_database_ddl_role ENCODING utf8 LC_COLLATE "C" LC_CTYPE "C" TEMPLATE template0;
 SELECT pg_get_database_ddl('test_get_database_ddl', false);
 DROP DATABASE "test_get_database_ddl";
-
 
 -- Test LOCAL_PROVIDER and BUILTIN_LOCALE for builtin type
 CREATE DATABASE "test_get_database_ddl_builtin"
@@ -40,14 +38,12 @@ CREATE DATABASE "test_get_database_ddl_builtin"
 SELECT pg_get_database_ddl('test_get_database_ddl_builtin', false);
 DROP DATABASE "test_get_database_ddl_builtin";
 
-
 -- Test ALLOW_CONNECTION and CONNECTION_LIMIT
 CREATE DATABASE "test_get_database_ddl_conn"
     OWNER test_database_ddl_role TEMPLATE template0 ENCODING 'UTF8'
     ALLOW_CONNECTIONS 0 CONNECTION LIMIT 50;
 SELECT pg_get_database_ddl('test_get_database_ddl_conn', false);
 DROP DATABASE "test_get_database_ddl_conn";
-
 
 -- With Pretty formatted
 \pset format unaligned
@@ -56,7 +52,6 @@ CREATE DATABASE "test_get_database_ddl"
 SELECT pg_get_database_ddl('test_get_database_ddl', true);
 DROP DATABASE "test_get_database_ddl";
 
-
 -- Test LOCAL_PROVIDER and BUILTIN_LOCALE for builtin type
 CREATE DATABASE "test_get_database_ddl_builtin"
     OWNER test_database_ddl_role TEMPLATE template0 ENCODING 'UTF8'
@@ -64,14 +59,12 @@ CREATE DATABASE "test_get_database_ddl_builtin"
 SELECT pg_get_database_ddl('test_get_database_ddl_builtin', true);
 DROP DATABASE "test_get_database_ddl_builtin";
 
-
 -- Test ALLOW_CONNECTION and CONNECTION_LIMIT
 CREATE DATABASE "test_get_database_ddl_conn"
     OWNER test_database_ddl_role TEMPLATE template0 ENCODING 'UTF8'
     ALLOW_CONNECTIONS 0 CONNECTION LIMIT 50;
 SELECT pg_get_database_ddl('test_get_database_ddl_conn', true);
 DROP DATABASE "test_get_database_ddl_conn";
-
 
 -- Clean up
 DROP ROLE test_database_ddl_role;
